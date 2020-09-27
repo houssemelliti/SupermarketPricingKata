ce1779add729e64203b18bb2519da2c0d8c1791f
Houssem Melliti <houssem.melliti13@gmail.com>
2020-09-27 15:10:00 +0100

    add View Models classes

---
+++ SupermarketPricingKata/ViewModels/Checkout.cs
@@ -0,0 +1,21 @@
+﻿using SupermarketPricingKata.Models;
+using System.Collections.Generic;
+
+namespace SupermarketPricingKata.ViewModels
+{
+    /// <summary>
+    /// View model representing the checkout object.
+    /// </summary>
+    public class Checkout
+    {
+        /// <summary>
+        /// Models the list of items in the checkout.
+        /// </summary>
+        public IList<CheckoutItem> CheckoutItems { get; set; }
+        
+        /// <summary>
+        /// Models the total price of the checkout.
+        /// </summary>
+        public decimal TotalPrice { get; set; }
+    }
+}
---
+++ SupermarketPricingKata/ViewModels/DiscountedCheckoutItem.cs
@@ -0,0 +1,25 @@
+﻿using SupermarketPricingKata.Models;
+
+namespace SupermarketPricingKata.ViewModels
+{
+    /// <summary>
+    /// Models a checkout item with integrated discount rule.
+    /// </summary>
+    public class DiscountedCheckoutItem
+    {
+        /// <summary>
+        /// Models the product object in the item.
+        /// </summary>
+        public Product Product { get; set; }
+        
+        /// <summary>
+        /// Models the quantity of <see cref="Models.Product"/> to be added to checkout.
+        /// </summary>
+        public decimal Quantity { get; set; }
+
+        /// <summary>
+        /// Models the <see cref="Models.DiscountRule"/> object associated with the <see cref="Models.Product"/>.
+        /// </summary>
+        public DiscountRule DiscountRule { get; set; }
+    }
+}
